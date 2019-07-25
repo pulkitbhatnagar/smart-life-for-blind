45f336979a8ecd6f69f8f088813fb0203bc65a3a
Pulkit bhatnagar <53307436+pulkitbhatnagar@users.noreply.github.com>
2019-07-25 20:43:06 +0530

    Add files via upload
    The prototype on which we were working was "The Smart Life for Blind". Our target users are those who lost their vision due to some accident.
    To solve their problems, we built two devices :-
    1-)Smart Stick 
    This device helps in navigation.
    The stick can be activated or deactivated simply on double tap. Once activated, UltraSonic sensors come into play which detects obstacles. SOS button on the top of the stick can send e-mail to relatives and friends in emergency condition.
    The mail consist of live location of the blind person along with an IP address for the video streaming of his/her surroundings. The live stream will be stored on the cloud.
    
    2)Smart Dustbin
    This device works on voice commands of the user which is connected to his smartphone.
    Blind person can say simple phrases like "dustbin come",  "dustbin go" and dustbin will follow those instructions.

---
+++ blindpeople.ino
@@ -0,0 +1,43 @@
+#include <Servo.h>
+Servo myservo;
+
+  const int pingTrigPin = 12; //Trigger connected to PIN 7   
+  const int pingEchoPin = 10; //Echo connected yo PIN 8   
+  int buz=7; //Buzzer to PIN 4   
+  void setup() {
+  myservo.attach(9);   
+  Serial.begin(9600);   
+  pinMode(buz, OUTPUT);  
+  digitalWrite(buz, HIGH); 
+  }   
+  void loop()   
+  { 
+  long duration, cm;   
+  pinMode(pingTrigPin, OUTPUT);   
+  digitalWrite(pingTrigPin, LOW);   
+  delayMicroseconds(2);   
+  digitalWrite(pingTrigPin, HIGH);   
+  delayMicroseconds(5);   
+  digitalWrite(pingTrigPin, LOW);   
+  pinMode(pingEchoPin, INPUT);   
+  duration = pulseIn(pingEchoPin, HIGH);   
+  cm = microsecondsToCentimeters(duration);   
+  if(cm<=15 && cm>0)   
+  {   
+  int d= map(cm, 1, 100, 20, 2000);   
+  digitalWrite(buz, LOW);
+  delay(500);
+  digitalWrite(buz,HIGH);
+  //digitalWrite(buz, HIGH);
+  }  
+  
+  Serial.print(cm);    
+  Serial.print("cm");   
+  Serial.println();   
+  delay(100);   
+  }   
+  long microsecondsToCentimeters(long microseconds)   
+  {   
+  return microseconds / 29 / 2;   
+  }   
+   
---
+++ blindstickbutton.ino
@@ -0,0 +1,45 @@
+#include <ESP8266WiFi.h>
+#include <ThingerESP8266.h>
+#include <SPI.h>
+#include <ThingerWifi.h>
+#define USERNAME "pulkitbhatnagar1"
+#define DEVICE_ID "node"
+#define DEVICE_CREDENTIAL "123456"
+
+#define SSID "Hamarawifi"
+#define SSID_PASSWORD "doomsday12"
+
+ThingerESP8266 thing(USERNAME, DEVICE_ID, DEVICE_CREDENTIAL);
+int pushPin = 12; //  Pin 6 on Node MCU   //the digital pin connected to the PIR sensor's output
+int ledPin = 4;  // Pin 2 on NodeMCU
+int val = 0;     // variable for reading the pin status
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(pushPin, INPUT_PULLUP);
+
+  int pushPin = 12; //  Pin 6 on Node MCU   //the digital pin connected to the PIR sensor's output
+int ledPin = 4;  // Pin 2 on NodeMCU
+int val = 0;     // variable for reading the pin status
+Serial.begin(9600);
+
+  thing.add_wifi(SSID, SSID_PASSWORD);
+
+  
+}
+
+void loop() {
+   val = digitalRead(pushPin);
+  Serial.println(val);// read input value
+  if (val == LOW) {         // check if the input is HIGH (button released)
+    digitalWrite(ledPin, HIGH);  // turn LED OFF
+    delay(1000);
+        digitalWrite(ledPin, LOW);
+  thing.handle();
+   thing.call_endpoint("email");
+           delay(5000);
+   // digitalWrite(inPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW); 
+}
+}
